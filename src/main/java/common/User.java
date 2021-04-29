55e205c6ab24fed7db95fba34201c7e0926ccd8f
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-04-29 17:07:56 +1000

    Begin implementing User class

--- src/main/java/common/User.java
+++ src/main/java/common/User.java
@@ -1,17 +1,29 @@
 package common;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
- *
+ * This class implements a user account.
  */
-public class User {
+public class User implements java.io.Serializable {
     /**
-     *
+     * Class version number.
      */
-    String username;
+    private static final long serialVersionUID = 0;
+
     /**
-     *
+     * An indication of whether the user is logged in or not.
      */
-    String password;
+    private boolean loggedIn;
+
+    /**
+     * A user's username.
+     */
+    private String username;
+
+    /**
+     * A user's hashed password.
+     */
+    private String password;
 
     /**
      *
@@ -20,6 +32,23 @@ public class User {
      */
     public User(String username, String password)
     {
+        this.username = username;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 
+    /**
+     * Indicate that user is logged in.
+     */
+    public void login()
+    {
+        loggedIn = true;
+    }
+
+    /**
+     * Indicate that user is not logged in.
+     */
+    public void logout()
+    {
+        loggedIn = false;
     }
 }
