9cce0e720f8b044cca77c961a66e817774fdf9e5
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-05-02 00:25:00 +1000

    Create user account superclass

---
+++ src/main/java/common/Credentials.java
@@ -0,0 +1,64 @@
+package common;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * This class represents a set of login credentials.
+ *
+ * @author Mitchell Egan
+ */
+public class Credentials implements java.io.Serializable {
+
+    /**
+     * The Class' version number.
+     */
+    private static final long serialVersionUID = 0;
+
+    /**
+     * The credentials' username.
+     */
+    private final String username;
+
+    /**
+     * The credentials' hashed password.
+     */
+    private String password;
+
+    /**
+     * Creates a Credentials object when a login is attempted.
+     *
+     * @param username A username.
+     * @param password A plaintext password.
+     */
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    /**
+     * Gets the credentials' username field.
+     *
+     * @return The credentials username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Gets the credentials' password field.
+     *
+     * @return The credentials' hashed password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the existing user's password field.
+     *
+     * @param password A plaintext password.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
