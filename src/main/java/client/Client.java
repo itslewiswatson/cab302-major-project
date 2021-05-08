9a241d6f930ed8c2df60cc283037ca2605fb4845
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-05-08 10:50:09 +1000

    Add arguments to main methods

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -8,7 +8,7 @@ public class Client
     /**
      *
      */
-    public static void main()
+    public static void main(String[] args)
     {
         System.out.println("I am the client.");
     }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -7,7 +7,7 @@ public class Server {
     /**
      *
      */
-    public static void main()
+    public static void main(String[] args)
     {
         System.out.println("I am the server.");
     }
