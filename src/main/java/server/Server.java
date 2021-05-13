6df271b9b3f85fd976d4babca42034e7369683b2
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-05-13 23:02:10 +1000

    Add construction of DBStatements and database closing

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,14 +1,16 @@
 package server;
 
-import common.NewUser;
-
 import java.io.IOException;
 
 /**
- *
+ * The system server.
  */
 public class Server {
     public static void main(String[] args) throws IOException {
+        DBStatements dbStatements = new DBStatements();
+
         System.in.read();
+
+        dbStatements.close();
     }
 }
