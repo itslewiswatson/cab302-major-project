902b243cbbb43a58cef316852e73e87a123a8d1e
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-05-15 02:18:10 +1000

    Implement GUI

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,34 +1,49 @@
 package client;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
 /**
- *
+ * This class is the client application.
  */
 public class Client extends Application {
+
     /**
      *
+     * Starts the JavaFX application.
+     *
+     * @param stage The initial stage.
+     */
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            stage.setTitle("Client");
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException exception) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "A program file has been deleted or become corrupted. Try restarting the client or rebuilding the program.", ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments.
      */
     public static void main(String[] args)
     {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.DARKGREY);
-
-        stage.setTitle("Client");
-        stage.setWidth(1280);
-        stage.setHeight(720);
-        stage.setResizable(false);
-
-        stage.setScene(scene);
-        stage.show();
     }
 }
