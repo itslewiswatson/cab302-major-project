2cf8cae7f1bebacc5e5fd451164396cced2a6138
Lewis Watson <noki@zorque.xyz>
2021-06-03 15:12:17 +1000

    Routes map test

---
+++ src/test/java/server/RoutesMapTest.java
@@ -0,0 +1,46 @@
+package server;
+
+import common.dto.DTO;
+import org.junit.Assert;
+import org.junit.Test;
+import server.handlers.Handler;
+
+import java.util.HashMap;
+
+public class RoutesMapTest {
+    private final RoutesMap routesMap;
+
+    public RoutesMapTest() {
+        this.routesMap = new RoutesMap();
+    }
+
+    @Test
+    public void testGetMap() {
+        HashMap<?, ?> routes = routesMap.getMap();
+        Assert.assertNotNull(routes);
+    }
+
+    @Test
+    public void testMapKeys() {
+        HashMap<?, ?> routes = routesMap.getMap();
+
+        for (Object routeDTO : routes.keySet()) {
+            Assert.assertTrue(routeDTO instanceof Class<?>);
+
+            Class<?> baseDTOClass = ((Class<?>) routeDTO).getSuperclass();
+            Assert.assertSame(DTO.class, baseDTOClass);
+        }
+    }
+
+    @Test
+    public void testMapValues() {
+        HashMap<?, ?> routes = routesMap.getMap();
+
+        for (Object routeHandler : routes.values()) {
+            Assert.assertTrue(routeHandler instanceof Class<?>);
+
+            Class<?> baseHandlerClass = ((Class<?>) routeHandler).getSuperclass();
+            Assert.assertSame(Handler.class, baseHandlerClass);
+        }
+    }
+}
