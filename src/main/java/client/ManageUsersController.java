20f1b139ec73240e2aa0ed7cbaaf6fd91e67cc62
Lewis Watson <noki@zorque.xyz>
2021-06-03 11:38:43 +1000

    Added manage users screen and empty controller

---
+++ src/main/java/client/ManageUsersController.java
@@ -0,0 +1,9 @@
+package client;
+
+public class ManageUsersController extends Controller {
+    public ManageUsersController(ClientController clientController) {
+        super(clientController);
+    }
+
+
+}
