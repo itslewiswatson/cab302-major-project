10bb43e6bdeb0603598c0476500c92dde9ebe767
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-06-04 17:54:03 +1000

    Implement displaying of historical trades and refreshing

--- src/main/java/client/UnitTradesController.java
+++ src/main/java/client/UnitTradesController.java
@@ -11,20 +11,21 @@ import common.dto.GetUnitTradesDTO;
 import common.dto.GetUnitsDTO;
 import common.dto.RemoveTradeDTO;
 import common.exceptions.NullResultException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.util.StringConverter;
 
 import java.io.IOException;
@@ -33,9 +34,8 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class UnitTradesController extends Controller implements Initializable {
-    public UnitTradesController(ClientController clientController) {
-        super(clientController);
-    }
+
+    Timeline refresher;
 
     @FXML
     private ComboBox<Unit> unitComboBox;
@@ -61,6 +61,16 @@ public class UnitTradesController extends Controller implements Initializable {
     @FXML
     private TableColumn<Trade, String> tradeType;
 
+    @FXML
+    private TableColumn<Trade, String> fulfilled;
+
+    @FXML
+    private Button removeTradeButton;
+
+    public UnitTradesController(ClientController clientController) {
+        super(clientController);
+    }
+
     private ArrayList<Trade> fetchUnitTrades(String unitId) {
         sendObject(new GetUnitTradesDTO(unitId));
         try {
@@ -82,11 +92,23 @@ public class UnitTradesController extends Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        int refreshPeriod = 5;
+
         setupColumns();
         setupRows();
         setupComboBox();
         populateUnitComboBox();
         populateTable();
+
+        refresher = new Timeline(
+                new KeyFrame(
+                        Duration.seconds(refreshPeriod),
+                        event -> populateTable()
+                )
+        );
+
+        refresher.setCycleCount(Timeline.INDEFINITE);
+        refresher.play();
     }
 
     private void setupComboBox() {
@@ -115,18 +137,38 @@ public class UnitTradesController extends Controller implements Initializable {
     public void populateTable() {
         Unit selectedUnit = unitComboBox.getValue();
         if (selectedUnit != null) {
-            ArrayList<Trade> trades = fetchUnitTrades(selectedUnit.getUnitId());
-            tableView.setItems(FXCollections.observableArrayList(trades));
+            int previouslySelectedTrade = tableView.getSelectionModel().getSelectedIndex();
+
+            ObservableList<Trade> trades = FXCollections.observableArrayList();
+
+            SortedList<Trade> sortedTrades = new SortedList<>(trades);
+
+            sortedTrades.comparatorProperty().bind(tableView.comparatorProperty());
+
+            tableView.setItems(sortedTrades);
+
+            trades.addAll(fetchUnitTrades(selectedUnit.getUnitId()));
+
+            tableView.getFocusModel().focus(previouslySelectedTrade);
+            tableView.getSelectionModel().select(previouslySelectedTrade);
         }
     }
 
     private void setupRows() {
         tableView.setRowFactory(tv -> {
             TableRow<Trade> row = new TableRow<>();
+
             row.setOnMouseClicked(mouseEvent -> {
-                if (!mouseEvent.isPrimaryButtonDown() && mouseEvent.getClickCount() != 2) return;
+                if (!mouseEvent.isPrimaryButtonDown() && mouseEvent.getClickCount() < 1) return;
                 Trade selectedTrade = tableView.getSelectionModel().getSelectedItem();
+
                 if (selectedTrade == null) return;
+
+                if (mouseEvent.getClickCount() != 2) {
+                    removeTradeButton.setDisable(selectedTrade.getDateFilled() != null);
+                    return;
+                }
+
                 viewTradeInfo(selectedTrade);
             });
             return row;
@@ -159,6 +201,10 @@ public class UnitTradesController extends Controller implements Initializable {
         quantity.setCellValueFactory(p -> new ReadOnlyObjectWrapper(p.getValue().getQuantity() - p.getValue().getQuantityFilled()));
         price.setCellValueFactory(p -> new ReadOnlyObjectWrapper(p.getValue().getPrice()));
         tradeType.setCellValueFactory(p -> new ReadOnlyObjectWrapper(p.getValue().getType()));
+        fulfilled.setCellValueFactory(p -> p.getValue().getDateFilled() != null ?
+                        new ReadOnlyObjectWrapper("Yes") :
+                        new ReadOnlyObjectWrapper("No")
+                );
     }
 
     @FXML
@@ -184,6 +230,7 @@ public class UnitTradesController extends Controller implements Initializable {
     }
 
     public void goBack() {
+        refresher.stop();
         switchToPage(Page.myAccount);
     }
 }
