053fff86396ce0f208f60a68c101a2bbc5981286
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-06-04 15:39:17 +1000

    Add client stage icon & title

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -2,6 +2,7 @@ package client;
 
 import client.strategy.ClientController;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +17,9 @@ public class Client extends Application {
      */
     @Override
     public void start(Stage stage) {
+        stage.getIcons().add(new Image("/icon.png"));
+        stage.setTitle("Electronic Asset Trading Platform");
+
         ClientController clientController = new ClientController(stage);
         clientController.initialise();
     }
