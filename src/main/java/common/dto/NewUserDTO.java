2a2c4aab1483b5c4f768f87f24dffea841a846c2
Lewis Watson <noki@zorque.xyz>
2021-06-06 23:39:53 +1000

    Missed javadoc added

--- src/main/java/common/dto/NewUserDTO.java
+++ src/main/java/common/dto/NewUserDTO.java
@@ -8,20 +8,42 @@ public class NewUserDTO extends DTO {
     private final String password;
     private final Boolean admin;
 
+    /**
+     * Create a new user DTO
+     *
+     * @param username Username string
+     * @param password Hashed password string
+     * @param admin Is admin boolean
+     */
     public NewUserDTO(String username, String password, Boolean admin) {
         this.username = username;
         this.password = password;
         this.admin = admin;
     }
 
+    /**
+     * Get username
+     *
+     * @return String of username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Get hashed password
+     *
+     * @return String of password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Get is admin
+     *
+     * @return Boolean showing whether is admin or not
+     */
     public Boolean isAdmin() {
         return admin;
     }
