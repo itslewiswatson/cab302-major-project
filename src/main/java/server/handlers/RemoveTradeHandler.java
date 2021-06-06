382dffcf84563cb2a6fc3fab95c2b5ae186813a7
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-06-06 12:52:16 +1000

    Add functionality to reimburse unfulfilled trade credits/assets when removing a trade

--- src/main/java/server/handlers/RemoveTradeHandler.java
+++ src/main/java/server/handlers/RemoveTradeHandler.java
@@ -1,6 +1,10 @@
 package server.handlers;
 
+import common.dataTypes.TradeType;
+import common.domain.Asset;
 import common.domain.Trade;
+import common.domain.Unit;
+import common.domain.UnitAsset;
 import common.dto.RemoveTradeDTO;
 import common.exceptions.NullResultException;
 import server.db.DBStrategy;
@@ -16,7 +20,25 @@ public class RemoveTradeHandler extends Handler<Boolean, RemoveTradeDTO> {
     public Boolean handle(RemoveTradeDTO dto) {
         try {
             Trade trade = resolveTrade(dto.getTradeId());
+            Unit unit = resolveUnit(trade.getUnit().getUnitId());
+            Asset asset = resolveAsset(trade.getAsset().getAssetId());
+            int remainingQuantity = trade.getQuantity() - trade.getQuantityFilled();
+
             dbStatements.removeTrade(trade);
+
+            if (trade.getType() == TradeType.BUY) {
+                unit.addCredits(remainingQuantity * trade.getPrice());
+                dbStatements.updateUnitCredits(unit);
+            }
+            else {
+                UnitAsset unitAsset = resolveUnitAsset(unit.getUnitId(), asset.getAssetId());
+                if (unitAsset == null) {
+                    return null;
+                }
+
+                unitAsset.addQuantity(remainingQuantity);
+                dbStatements.updateUnitAsset(unitAsset);
+            }
         } catch (NullResultException | SQLException e) {
             return false;
         }
