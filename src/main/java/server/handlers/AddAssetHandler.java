e0083fe6fcbded5e121e98002f165a287521e5d2
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-06-06 14:39:48 +1000

    Add returning of null DTO if asset with the same name already exists

--- src/main/java/server/handlers/AddAssetHandler.java
+++ src/main/java/server/handlers/AddAssetHandler.java
@@ -4,6 +4,7 @@ import common.domain.Asset;
 import common.domain.FullAsset;
 import common.dto.AddAssetDTO;
 import common.services.UuidGenerator;
+import org.jetbrains.annotations.Nullable;
 import server.db.DBStrategy;
 
 import java.util.ArrayList;
@@ -16,7 +17,15 @@ public class AddAssetHandler extends Handler<ArrayList<FullAsset>, AddAssetDTO> 
     @Override
     public ArrayList<FullAsset> handle(AddAssetDTO dto) {
         Asset newAsset = new Asset(UuidGenerator.generateUuid(), dto.getAssetName());
+
+        if (assetExists(newAsset.getAssetName())) return null;
+
         dbStatements.addAsset(newAsset);
         return dbStatements.findAssets();
+    }
+
+    private boolean assetExists(String assetName) {
+        @Nullable Asset asset = dbStatements.findAssetByName(assetName);
+        return asset != null;
     }
 }
