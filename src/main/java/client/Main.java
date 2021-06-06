5a178a295f267b04514b0af842752a571dad92e1
Mitchell Egan <39621820+mitchelljqegan@users.noreply.github.com>
2021-06-06 21:19:16 +1000

    Add JavaDoc comments

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,6 +1,14 @@
 package client;
 
+/**
+ * Main class.
+ */
 public class Main {
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args)
     {
         Client.main(args);
